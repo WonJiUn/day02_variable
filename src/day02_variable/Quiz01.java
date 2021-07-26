f29c6a51486b9b770ab132b05abb1a2b67d0bfda
wonjiun <sallyjelly2@naver.com>
2021-07-26 12:43:27 +0900

    기본 변수의 이해

---
+++ src/day02_variable/Quiz01.java
@@ -0,0 +1,27 @@
+package day02_variable;
+
+public class Quiz01 {
+	public static void main(String[] args) {
+	
+		byte b = 97;
+		short s = 20;
+		char c = 'A';
+		float f;
+		s = b;
+		System.out.println(s);
+		c = (char)b;
+		System.out.println(c);
+		s = (short)c;
+		System.out.println(s);
+		c = (char)s;
+		System.out.println(c);
+		f = (float)5.11;
+		f = 5.11f;
+		System.out.println(f);
+		
+		
+		
+		
+		
+	}
+}
