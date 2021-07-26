d002e34e20821f9dc10412aab14bbb9cd15ce5f6
wonjiun <sallyjelly2@naver.com>
2021-07-26 14:01:29 +0900

    Ex8까지 완료

---
+++ src/day02_variable/Ex05.java
@@ -0,0 +1,18 @@
+package day02_variable;
+
+public class Ex05 {
+	public static void main(String[] args) {
+	
+		//변수의 상수화
+		final int i = 100;
+		//i = 200;
+		System.out.println(i);
+		
+		//상수화할때는 변수명의 모든 글자를 대문자로 작성한다는 규칙이 있다
+		final String KOREA = "대한민국";
+		//KOREA = "미국";
+		System.out.println(KOREA);
+		
+		
+	}
+}
